be1d7ad6771af5622037b63accd833396d764e57
alex9932 <alex9932@alex9932-Z68AP-D3>
2021-11-10 02:41:29 +0300

    1.6 pre 1

--- src/alex9932/delta/Main.java
+++ src/alex9932/delta/Main.java
@@ -1,47 +1,38 @@
 package alex9932.delta;
 
-import java.io.FileInputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.security.KeyStore;
-import java.util.Arrays;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-
 // https://github.com/Alex9932/DeltaServer
 
 public class Main {
 	public static final String SERVER     = "Deltaserver";
-	public static final String VERSION    = "1.5";
-	public static final String BUILD_NAME = "Shiro";
+	public static final String VERSION    = "1.6 pre 1";
+	public static final String BUILD_NAME = "Kaito";
 	
 	public static String PLATFORM = "none";
 	public static int SERVER_PORT = 80;
 	public static int SECURE_PORT = 443;
 	public static boolean running = true;
 
-	private static boolean isHTTP = false;
-	private static boolean isHTTPS = true;
+	private static boolean isHTTP = true;
+	private static boolean isHTTPS = false;
 
+	//-secure -nohttp -ports 8443
 	public static void main(String[] args) {
-		System.out.println(SERVER + " " + VERSION + " (" + BUILD_NAME + ") is starting up...");
-
 		PLATFORM = String.valueOf(
 				System.getProperty("os.name")) +
 				" " + System.getProperty("os.version") +
 				" " + System.getProperty("os.arch");
+		if(parseCommandLine(args) != 0)
+			System.exit(-1); // Exit
 		
 		System.out.println("Platform: " + PLATFORM);
 
+		System.out.println(SERVER + " " + VERSION + " (" + BUILD_NAME + ") is starting up...");
 		try {
 			if(isHTTP)
-				new Server(getSocket()).getThread().start();
+				Server.startHTTP();
 			
 			if(isHTTPS)
-				new Server(getSecureSocket(new InetSocketAddress("0.0.0.0", SECURE_PORT))).getThread().start();
+				Server.startHTTPS();
 		} catch (Exception e) {
 			Main.running = false;
 			System.out.println("ERR: " + e);
@@ -49,29 +40,64 @@ public class Main {
 		}
 	}
 	
-	private static ServerSocket getSocket() throws Exception {
-		return new ServerSocket(SERVER_PORT);
+	private static int parseCommandLine(String[] args) {
+		if(args.length == 0) {
+			printHelp();
+			return -1;
+		}
+		
+		for (int i = 0; i < args.length; i++) {
+			if(args[i].equals("-secure")) {
+				isHTTPS = true;
+			} else if(args[i].equals("-nohttp")) {
+				isHTTP = false;
+			} else if(args[i].equals("-port")) {
+				if(i + 1 >= args.length) {
+					System.out.println("Invalid argument: " + args[i]);
+					printHelp();
+					return -1;
+				}
+				SERVER_PORT = Integer.parseInt(args[i + 1]);
+				i++;
+			} else if(args[i].equals("-ports")) {
+				if(i + 1 >= args.length) {
+					System.out.println("Invalid argument: " + args[i]);
+					printHelp();
+					return -1;
+				}
+				SECURE_PORT = Integer.parseInt(args[i + 1]);
+				i++;
+			} else if(args[i].equals("-h") || args[i].equals("-?") || args[i].equals("-help")) {
+				printHelp();
+				return -1;
+			} else if(args[i].equals("-v") || args[i].equals("-version")) {
+				printVersion();
+				return -1;
+			} else {
+				System.out.println("Invalid argument: " + args[i]);
+				printHelp();
+				return -1;
+			}
+		}
+		
+		return 0;
 	}
-	
-	private static ServerSocket getSecureSocket(InetSocketAddress address) throws Exception {
-		int backlog = 0;
-		char[] keyStorePassword = "certpassword".toCharArray();                 // USED SELFSIGNED CERTIFICATE
-		Path storePath = FileSystems.getDefault().getPath(".", "keystore.jks"); // USED SELFSIGNED CERTIFICATE
 
-		ServerSocket serverSocket = getSslContext(storePath, keyStorePassword)
-				.getServerSocketFactory()
-				.createServerSocket(address.getPort(), backlog, address.getAddress());
-		Arrays.fill(keyStorePassword, '0');
-		return serverSocket;
+	private static void printHelp() {
+		System.out.println("~~~~~ " + SERVER + " " + VERSION + " (" + BUILD_NAME + ") ~~~~~");
+		System.out.println(" -? / -h / -help      Print this help message");
+		System.out.println(" -v / -version        Print version");
+		System.out.println(" -secure              Use HTTPS");
+		System.out.println(" -nohttp              Disable HTTP");
+		System.out.println(" -port <http port>    Set HTTP server port (default 80)");
+		System.out.println(" -ports <https port>  Set HTTPS server port (default 443)");
+		System.out.println("");
+		System.out.println("Usage:");
+		System.out.println(" java -jar server.jar -secure -nohttp -port 8080 -ports 8443");
 	}
 
-	private static SSLContext getSslContext(Path keyStorePath, char[] keyStorePass) throws Exception {
-		KeyStore keyStore = KeyStore.getInstance("JKS");
-		keyStore.load(new FileInputStream(keyStorePath.toFile()), keyStorePass);
-		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-		keyManagerFactory.init(keyStore, keyStorePass);
-		SSLContext sslContext = SSLContext.getInstance("TLS");
-		sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
-		return sslContext;
+	private static void printVersion() {
+		System.out.println(SERVER + " " + VERSION + " (" + BUILD_NAME + ")");
+		System.out.println("Runned on " + PLATFORM);
 	}
 }
--- src/alex9932/delta/Server.java
+++ src/alex9932/delta/Server.java
@@ -1,7 +1,15 @@
 package alex9932.delta;
 
+import java.io.FileInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.util.Arrays;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 public class Server implements Runnable {
 	private ServerSocket server;
@@ -30,5 +38,39 @@ public class Server implements Runnable {
 	
 	public Thread getThread() {
 		return thread;
+	}
+
+	public static void startHTTP() throws Exception {
+		new Server(getSocket()).getThread().start();
+	}
+
+	public static void startHTTPS() throws Exception {
+		new Server(getSecureSocket()).getThread().start();
+	}
+
+	private static ServerSocket getSocket() throws Exception {
+		return new ServerSocket(Main.SERVER_PORT);
+	}
+	
+	private static ServerSocket getSecureSocket() throws Exception {
+		int backlog = 0;
+		char[] keyStorePassword = "certpassword".toCharArray();                 // USED SELFSIGNED CERTIFICATE
+		Path storePath = FileSystems.getDefault().getPath(".", "keystore.jks"); // USED SELFSIGNED CERTIFICATE
+
+		ServerSocket serverSocket = getSslContext(storePath, keyStorePassword)
+			.getServerSocketFactory()
+			.createServerSocket(Main.SECURE_PORT, backlog);
+		Arrays.fill(keyStorePassword, '0');
+		return serverSocket;
+	}
+
+	private static SSLContext getSslContext(Path keyStorePath, char[] keyStorePass) throws Exception {
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(new FileInputStream(keyStorePath.toFile()), keyStorePass);
+		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+		keyManagerFactory.init(keyStore, keyStorePass);
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+		return sslContext;
 	}
 }
